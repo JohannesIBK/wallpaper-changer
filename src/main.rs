998708ee5f2a1713863d1fb7d3070a78dbbdace1
JohannesIBK <mail@greuter.dev>
2022-11-24 14:48:42 +0100

    remove unused stuff

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,6 @@ mod changer;
 
 
 fn main() {
-    match updater::update_application() {
-        Ok(_) => println!("Updated application"),
-        Err(e) => println!("Error updating application: {e}"),
-    } ;
     let result = changer::run();
 
     match result {
